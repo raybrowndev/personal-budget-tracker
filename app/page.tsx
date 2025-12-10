6882cf95336bc2428f5b229b05b870b4df805439
Ray Brown <raybrown.dev@gmail.com>
2025-12-10 08:28:35 +0000

    react icons added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image"; // Image class form next to add 
 import { Button } from "@/components/ui/button"
 import { Quicksand } from "next/font/google"; 
+import { FaTv } from "react-icons/fa";
  
 
 // PAGE START
@@ -12,30 +13,27 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+
+<div className="p-6">
+      <div className="h-12 w-12 rounded-full bg-slate-200 flex items-center justify-center">
+        <FaTv className="text-slate-900 text-xl" />
         <h1 className={quicksand.variable}>
-          <span className="text-1xl font-bold text-emerald-500">
+          <span className="text-2xl font-bold text-emerald-500 text-shadow-xs p-2">
             Personal Budget Tracker
             </span>
         </h1>
+      </div>
+    </div>
+
         {/* <Image
           className="dark:invert"
           src="/pbt_logo.webp"
           alt="Personal Budget App logo"
-          width={500}
+          width={50}
           height={20}
           priority
         /> */}
   
-        <div className="flex flex-wrap items-center gap-2 md:flex-row">
-         <a
-              href="https://github.com/raybrowndev"
-              className="font-medium text-zinc-950 "
-            >
-              <Button variant="outline">Created by Ray Brown 👋</Button>
-            </a>{" "}
-      
-
-    </div>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-4xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Personal Budget Tracker 2.0
@@ -46,31 +44,16 @@ export default function Home() {
           </p>
 
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-10 w-full items-center justify-center gap-2 rounded-lg bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-10 w-full items-center justify-center rounded-lg border border-solid md:w-[158px] hover:bg-pink-300"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+                <div className="flex flex-wrap items-center gap-2 md:flex-row">
+         <a
+              href="https://github.com/raybrowndev"
+              className="font-medium text-zinc-950 "
+            >
+              <Button variant="outline">Created by Ray Brown 👋</Button>
+            </a>{" "}
+      
+
+    </div>
       </main>
     </div>
   );
